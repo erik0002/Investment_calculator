482b93fe139a4e7c2edd67dfb41aad9ca0bc73e7
Yeliazar <eleazarkazhuro20@gmail.com>
2025-04-09 22:24:00 +0300

    add: condition display information when data incorrect

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import UserInput from "./components/UserInput";
 import { useState } from "react";
 
 function App() {
-
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
+
+  const inputIsValid = userInput.duration >= 1;
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
@@ -25,7 +26,10 @@ function App() {
     <>
       <Header />
       <UserInput onChangeInput={handleChange} userInput={userInput} />
-      <Results  userInput={userInput}/>
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results userInput={userInput} />}
     </>
   );
 }
